7b44e42bcec44e8beb9cdb11ae8e3c305f91b3e6
Tarun Thakur <thakur.cs.tarun@gmail.com>
2022-05-24 20:12:36 +0530

    parsing token

--- jwt/main.go
+++ jwt/main.go
@@ -18,8 +18,6 @@ type UserClaims struct {
 	SessionID          int64
 }
 
-
-
 // Structured version of Claims Section, as referenced at
 // https://tools.ietf.org/html/rfc7519#section-4.1
 // See examples for how to use this with your own claim types
@@ -33,11 +31,9 @@ type UserClaims struct {
 //	Subject   string `json:"sub,omitempty"`
 // }
 
-
-
 // Validating the claims
-// the reason we are using a pointer to UserClaims is bcoz the jwt.StandardClaims uses a pointer 
-func (u *UserClaims) Valid() error { // checks if the token is expired or not 
+// the reason we are using a pointer to UserClaims is bcoz the jwt.StandardClaims uses a pointer
+func (u *UserClaims) Valid() error { // checks if the token is expired or not
 	// Compares the exp claim against cmp.
 	// If required is false, this method will return true if the value matches or is unset
 	//func (c *StandardClaims) VerifyExpiresAt(cmp int64, req bool) bool {
@@ -51,10 +47,50 @@ func (u *UserClaims) Valid() error { // checks if the token is expired or not
 	if u.SessionID == 0 {
 		return fmt.Errorf("invalid session id")
 	}
-	return nil 
+	return nil
 }
 
+func GenerateKey() []byte {
+	var key = []byte{}
 
-func createToken(c * UserClaims)(token string, err error) {
-  jwt.NewWithClaims()
+	for i := 1; i <= 63; i++ {
+		key = append(key, byte(i))
+	}
+
+	return key
+}
+
+func createToken(c *UserClaims) (token string, err error) {
+	// this will create a base token
+	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c) // this requires a signing method and claims
+	signedToken, err := t.SignedString(GenerateKey())
+
+	return signedToken, nil
+}
+
+func parseToken(signedToken string)(claims *UserClaims,err error){
+	
+	// it parses the token without verifying and then passes the token into the function defined inside the function to verify it. If 
+	// the token is verified, then it returns the token 
+	t,err:=jwt.ParseWithClaims(signedToken,claims,func (t *jwt.Token)(interface{},error){
+    // first we are checking whether thhe signing method of the token passed is equal to the expected one    
+    // it is a good practice to verifu the algo 
+    if t.Method.Alg()!= jwt.SigningMethodHS512.Alg(){
+    	return nil, fmt.Errorf("Invalid signing algorithm")
+    }
+
+    return GenerateKey(),nil 
+	} )
+
+	if err!=nil{
+		return nil, fmt.Errorf("Error in parse token: %w",err)
+	}
+
+	 if !t.Valid{
+	 	 return nil,fmt.Errorf("token not valid: %w",err)
+	 }
+
+   // t.Claims is an interface so we have to assert the type to *UserClaims
+   claims = t.Claims.(*UserClaims)
+   return claims ,nil 
 }
