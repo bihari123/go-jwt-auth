feb131a24568b850147d6c7564db426fb4ddc2d7
Tarun Thakur <thakur.cs.tarun@gmail.com>
2022-05-24 15:08:18 +0530

    json encode

---
+++ json-encoding-decoding/part-3/go.mod
@@ -0,0 +1,3 @@
+module github.com/bihari123/go-jwt-auth/json-encoding-decoding/part-3
+
+go 1.18
---
+++ json-encoding-decoding/part-3/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type person struct {
+	First string `json:"First"`
+}
+
+func encode(w http.ResponseWriter, r *http.Request) {
+
+	p1 := person{
+		First: "Jenny",
+	}
+
+	p2 := person{
+		First: "James",
+	}
+
+	xp := []person{p1, p2}
+
+  // the diffference between the Marshal and encoder is that encoder encoded the data into json on the go whereas marshal takes the full data and then encodes.
+  // Encoder is helpful in writing rest apis as it is memory efficient
+  err:=json.NewEncoder(w).Encode(xp)
+
+  if err!=nil{
+  	log.Println(fmt.Errorf("encoded bad data: %w",err))
+  }
+
+}
+
+func decode(w http.ResponseWriter, r *http.Request) {
+	var bs []byte
+	xp := []person{}
+
+	err := json.Unmarshal(bs, &xp)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	fmt.Print("\n\n", xp)
+
+}
+
+func main(){
+	http.HandleFunc("/encode",encode)
+	http.HandleFunc("/decode",decode)
+	http.ListenAndServe(":8080",nil)
+}
